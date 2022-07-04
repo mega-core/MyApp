a03f6d33087f9179c13e1ad23d0e71447276f85f
Sepehr <sepehrayobi222@yahoo.com>
2022-07-04 21:28:19 +0430

    gradle upgrade

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.widget.LinearLayoutCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.splashScreenTheme)
         setContentView(R.layout.activity_main)
-
-        Toast.makeText(this,"Hello World!",Toast.LENGTH_SHORT).show()
+        val nothingBtn = findViewById<ImageView>(R.id.nothing)
+        val goToNextActivity = findViewById<LinearLayoutCompat>(R.id.linearLayoutCompat)
+        nothingBtn.setOnClickListener{
+            Toast.makeText(this,"This button does nothing",Toast.LENGTH_SHORT).show()
+        }
+        goToNextActivity.setOnClickListener{
+            val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("Text","Hi whats up")
+            startActivity(intent)
+        }
     }
 }
