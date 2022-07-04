82ed686905b83ead7e18193b6ccaeb7acd5a84d1
Sepehr <sepehrayobi222@yahoo.com>
2022-07-05 01:10:19 +0430

    New Update

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-import androidx.appcompat.widget.LinearLayoutCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,22 +11,20 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.splashScreenTheme)
         setContentView(R.layout.activity_main)
         val textV = findViewById<EditText>(R.id.editTextTextPersonName)
-        val nextButton = findViewById<LinearLayoutCompat>(R.id.linearLayoutCompat)
         val profileImg = findViewById<ImageView>(R.id.profile_img)
         val nothing = findViewById<ImageButton>(R.id.nothing)
+        val nextButton = findViewById<LinearLayout>(R.id.nextBtn)
         nothing.setOnClickListener{
             Toast.makeText(this,"This button does nothing",Toast.LENGTH_LONG).show()
         }
         profileImg.setOnClickListener{
             Toast.makeText(this,"There is no image",Toast.LENGTH_SHORT).show()
         }
-        nextButton.setOnClickListener {
-            val name = textV.text?.toString()
+        nextButton.setOnClickListener{
+            val name = textV.text.toString()
             Intent(this,SecondActivity::class.java).also {
                 it.putExtra("EXTRA_TEXT",name)
-                startActivity(it)
-
-            }
+                startActivity(it) }
+        }
         }
     }
-}
